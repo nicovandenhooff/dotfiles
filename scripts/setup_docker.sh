8e559e832b3d5f098e10154f7ec45cd43d7e5e16
Nico Van den Hooff <nico.vandenhooff@xtractone.com>
2023-11-13 22:07:07 -0800

    Fix docker setup script

--- scripts/setup_docker.sh
+++ scripts/setup_docker.sh
@@ -6,16 +6,18 @@ set -e
 # update package index
 echo "Updating package index..."
 sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
 
-# add Docker's official GPG key
-echo "Adding Docker's GPG key..."
-sudo install -y -m 0755 -d /etc/apt/keyrings
+# Add Docker's official GPG key:
+sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-# add the Docker repository to Apt sources
-echo "Adding Docker repository..."
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo $VERSION_CODENAME) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
 # install Docker packages
