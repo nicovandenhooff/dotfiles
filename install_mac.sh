9239f908cc03d9546e48b2a59199f20a6ad10a5d
Nico Van den Hooff <nicovandenhooff@gmail.com>
2025-11-30 15:20:01 -0800

    clean up install scripts

--- install_mac.sh
+++ install_mac.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -e
+set -eux
 
 # directory where script and dotfiles are located
-DOTFILES_DIR=$(dirname "$(readlink -f "$0")")
+DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 # create symlinks
 ln -sf "$DOTFILES_DIR/.bash_aliases_mac" ~/.bash_aliases
 ln -sf "$DOTFILES_DIR/.bash_profile" ~/.bash_profile
 ln -sf "$DOTFILES_DIR/.tmux.conf" ~/.tmux.conf
+ln -sf "$DOTFILES_DIR/Brewfile" ~/.Brewfile
